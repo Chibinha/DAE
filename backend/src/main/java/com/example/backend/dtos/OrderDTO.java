e1eb208ec406fef3d49bb787c372671067a255d8
Luigi <Lmname999@gmail.com>
2024-01-15 17:24:36 +0000

    Entity Changes

--- backend/src/main/java/com/example/backend/dtos/OrderDTO.java
+++ backend/src/main/java/com/example/backend/dtos/OrderDTO.java
@@ -1,4 +1,78 @@
 package com.example.backend.dtos;
 
+import com.example.backend.entities.Client;
+import com.example.backend.entities.PhysicalProduct;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class OrderDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long idOrder;
+    @Temporal(TemporalType.DATE)
+    private Date orderDate;
+    @NotNull
+    private String orderType;
+    private String materialType;
+    @OneToMany(mappedBy = "product")
+    private List<PhysicalProduct> products;
+    /*//@OneToMany(mappedBy = "observation")
+    //private List<Observation> observation;
+    @OneToMany(mappedBy = "client")
+    private List<Client> client;*/
+    public OrderDTO() {
+        this.products = new ArrayList<>();
+    }
+
+    public OrderDTO(long idOrder, Date orderDate, String orderType, String materialType) {
+        this.idOrder = idOrder;
+        this.orderDate = orderDate;
+        this.orderType = orderType;
+        this.materialType = materialType;
+        this.products = new ArrayList<>();
+    }
+
+    public long getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(long idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public String getOrderType() {
+        return orderType;
+    }
+
+    public void setOrderType(String orderType) {
+        this.orderType = orderType;
+    }
+
+    public String getMaterialType() {
+        return materialType;
+    }
+
+    public void setMaterialType(String materialType) {
+        this.materialType = materialType;
+    }
+
+    public List<PhysicalProduct> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<PhysicalProduct> products) {
+        this.products = products;
+    }
 }
--- backend/src/main/java/com/example/backend/entities/Order.java
+++ backend/src/main/java/com/example/backend/entities/Order.java
@@ -4,39 +4,58 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
+import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @Entity
-@Table(name = "orders")
+@Table(
+        name = "orders",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"idOrder"})
+)
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllOrders",
+                query = "SELECT o FROM Order o ORDER BY o.idOrder"
+        )
+})
 public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long idOrder;
-    @Temporal(TemporalType.DATE)
-    private Date orderDate;
+
     @NotNull
+    @Column(name = "orderType")
     private String orderType;
+
+    @Column(name = "materialType")
     private String materialType;
+
     @OneToMany(mappedBy = "product")
     private List<PhysicalProduct> products;
-    //@OneToMany(mappedBy = "observation")
-    //private List<Observation> observation;
-    @OneToMany(mappedBy = "client")
-    private List<Client> client;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    @NotNull
+    public Client client;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "orderTimestamp")
+    @NotNull
+    private Timestamp orderTimestamp;
+
     public Order() {
         this.products = new ArrayList<>();
-        this.client = new ArrayList<>();
+        //this.orderTimestamp = new Timestamp(System.currentTimeMillis());
     }
 
-    public Order(long idOrder, Date orderDate, String orderType, String materialType) {
+    public Order(long idOrder, String orderType, String materialType, Client client) {
         this.idOrder = idOrder;
-        this.orderDate = orderDate;
         this.orderType = orderType;
         this.materialType = materialType;
         this.products = new ArrayList<>();
-        this.client = new ArrayList<>();
+        this.orderTimestamp = new Timestamp(System.currentTimeMillis());
+        this.client = new Client();
     }
 
     public long getIdOrder() {
@@ -45,14 +64,6 @@ public class Order implements Serializable {
 
     public void setIdOrder(long idOrder) {
         this.idOrder = idOrder;
-    }
-
-    public Date getOrderDate() {
-        return orderDate;
-    }
-
-    public void setOrderDate(Date orderDate) {
-        this.orderDate = orderDate;
     }
 
     public String getOrderType() {
@@ -79,11 +90,11 @@ public class Order implements Serializable {
         this.products = products;
     }
 
-    public List<Client> getClient() {
-        return client;
+    public Timestamp getOrderTimestamp() {
+        return orderTimestamp;
     }
 
-    public void setClient(List<Client> client) {
-        this.client = client;
+    public void setOrderTimestamp(Timestamp orderTimestamp) {
+        this.orderTimestamp = orderTimestamp;
     }
 }
