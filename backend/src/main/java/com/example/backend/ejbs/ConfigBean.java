e56c7e0b0342689229742c7421276649fe6967b9
Alberto Pingo <albertopingobusiness@gmail.com>
2024-01-17 05:28:14 +0000

    Fix websocket pending messages

--- backend/src/main/java/com/example/backend/ejbs/ConfigBean.java
+++ backend/src/main/java/com/example/backend/ejbs/ConfigBean.java
@@ -1,12 +1,10 @@
 package com.example.backend.ejbs;
 
-import com.example.backend.entities.Maker;
 import jakarta.annotation.PostConstruct;
 import jakarta.ejb.EJB;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.logging.Logger;
 
@@ -14,55 +12,86 @@ import java.util.logging.Logger;
 @Startup
 public class ConfigBean {
     @EJB
+    private MakerBean makerBean;
+    @EJB
+    private ClientBean clientBean;
+    @EJB
+    private LineOperatorBean lineOperatorBean;
+    @EJB
     private ProductBean productBean;
     @EJB
     private PhysicalProductBean physicalProductBean;
     @EJB
-    private ClientBean clientBean;
+    private SensorBean sensorBean;
     @EJB
-    private MakerBean makerBean;
+    private ObservationBean observationBean;
     @EJB
-    private LineOperatorBean lineOperatorBean;
+    private PackageBean packageBean;
+    @EJB
+    private ProductPackageBean productPackageBean;
+    @EJB
+    private TransportationPackageBean transportationPackageBean;
     @EJB
     private OrderBean orderBean;
+
 
     private static final Logger logger = Logger.getLogger("ejbs.ConfigBean");
 
     @PostConstruct
     public void populateDB() {
         try {
-            //String username, String password, String name, String email
+            //#region Users
             makerBean.create("maker1", "123", "maker1", "maker1@mail.pt");
             makerBean.create("maker2", "123", "maker2", "maker2@mail.pt");
             makerBean.create("maker3", "123", "maker3", "maker3@mail.pt");
 
+            clientBean.create("joao", "123", "joao", "joao@mail.com");
+            clientBean.create("bruno", "123", "bruno", "bruno@mail.com");
+
+            lineOperatorBean.create("diogo", "123", "diogo", "diogo@mail.com");
+            lineOperatorBean.create("marco", "123", "MARCO", "marco@mail.com");
+            //#endregion
+
+            //#region Products/PhysicalProducts
             productBean.create("Pizza", 10, "Pizza de queijo", 0.5, "Queijo", "maker1");
             productBean.create("Hamburger", 5, "Hamburger de carne", 0.5, "Carne", "maker2");
             productBean.create("Coca-Cola", 1, "Coca-Cola", 0.5, "Coca-Cola", "maker3");
 
-
-            // public long create(String serialNumber, long productId)
             physicalProductBean.create("123456781", 1);
             physicalProductBean.create("187654322", 1);
             physicalProductBean.create("287654321", 2);
             physicalProductBean.create("223456782", 2);
             physicalProductBean.create("323456781", 3);
             physicalProductBean.create("387654322", 3);
+            //#endregion
+
+            //#region Sensors
+            sensorBean.create("sensor1", "type1");
+            sensorBean.create("sensor2", "type2");
+            sensorBean.create("sensor3", "type3");
+            //#endregion
+
+            //#region Observations
+            observationBean.create("type1", "value1", "unit1", 1);
+            observationBean.create("type2", "value2", "unit2", 1);
+            observationBean.create("type3", "value3", "unit3", 2);
+            observationBean.create("type4", "value4", "unit4", 2);
+            observationBean.create("type5", "value5", "unit5", 3);
+            observationBean.create("type6", "value6", "unit6", 3);
+            //#endregion
 
 
-            clientBean.create("joao", "123", "joao", "joao@mail.com");
-            clientBean.create("bruno", "123", "bruno", "bruno@mail.com");
-            lineOperatorBean.create("diogo", "123", "diogo", "diogo@mail.com");
-            lineOperatorBean.create("marco", "123", "MARCO", "marco@mail.com");
+            //#region Packages
+            //#endregion
+
+            //#region Orders
             HashMap<Long, Integer> order1 = new HashMap<Long, Integer>();
             order1.put(1L, 2);
             order1.put(2L,3);
             order1.put(3L, 1);
             orderBean.create("Primária", "marco", "bruno", order1);
             //orderBean.getAllProductsForOrder(1);
-
-            //orderBean.create(1, date.toString(2024.01.11), "Alfredo", "Joaquim", "Delivered");
-
+            //#endregion
         } catch (Exception e) {
             logger.severe(e.getClass().getCanonicalName() + e.getMessage());
         }
--- backend/src/main/java/com/example/backend/websocket/WebsocketService.java
+++ backend/src/main/java/com/example/backend/websocket/WebsocketService.java
@@ -14,47 +14,50 @@ import java.util.logging.Logger;
 @Startup
 @ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 public class WebsocketService {
-        //logger
-        private static final Logger logger = Logger.getLogger(WebsocketService.class.getName());
-        private static final Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
-        private static final Map<String, List<String>> pendingMessages = new ConcurrentHashMap<>();
+    //logger
+    private static final Logger logger = Logger.getLogger(WebsocketService.class.getName());
+    private static final Map<String, Session> onlineSessions = new ConcurrentHashMap<>();
+    private static final Map<String, List<String>> pendingMessages = new ConcurrentHashMap<>();
 
-        public static void addSession(String username, Session session) {
-            onlineSessions.put(username, session);
-            logger.log(Level.INFO, "Session added for user: {0}", username);
-            sendPendingMessages(username);
-            logger.log(Level.INFO, "Pending messages sent to user: {0}", username);
+    public static void addSession(String username, Session session) {
+        onlineSessions.put(username, session);
+        logger.log(Level.INFO, "Session added for user: {0}", username);
+        sendPendingMessages(username);
+        logger.log(Level.INFO, "Pending messages sent to user: {0}", username);
+    }
+
+    public static void removeSession(String username) {
+        onlineSessions.remove(username);
+        logger.log(Level.INFO, "Session removed for user: {0}", username);
+    }
+
+    public static void sendNotification(String username, String message) {
+        var session = onlineSessions.get(username);
+        if (session != null) {
+            logger.log(Level.INFO, "Sending notification to user: {0} - {1}", new Object[]{username, message});
+            session.getAsyncRemote().sendText(message);
+        } else {
+            pendingMessages.computeIfAbsent(username, k -> new ArrayList<>()).add(message);
         }
+    }
 
-        public static void removeSession(String username) {
-            onlineSessions.remove(username);
-            logger.log(Level.INFO, "Session removed for user: {0}", username);
-        }
+    public static void sendPendingMessages(String username) {
+        var session = onlineSessions.get(username);
+        if (session != null) {
+            var messages = pendingMessages.get(username);
+            if (messages != null) {
 
-        public static void sendNotification(String username, String message) {
-            var session = onlineSessions.get(username);
-            if (session != null) {
-                logger.log(Level.INFO, "Sending notification to user: {0} - {1}", new Object[]{username, message});
-                session.getAsyncRemote().sendText(message);
-            } else {
-                pendingMessages.computeIfAbsent(username, k -> new ArrayList<>()).add(message);
-            }
-        }
-
-        public static void sendPendingMessages(String username) {
-            var session = onlineSessions.get(username);
-            if (session != null) {
-                var messages = pendingMessages.get(username);
-                logger.log(Level.INFO, "Sending {0} pending messages to user: {1}", new Object[]{messages.size(), username});
                 for (var message : messages) {
                     session.getAsyncRemote().sendText(message);
                     logger.log(Level.INFO, "Sent [{0}] Pending message to user: {1} - {2}", new Object[]{messages.indexOf(message), username, message});
-                    pendingMessages.remove(username);
                 }
+                pendingMessages.remove(username);
             }
         }
+    }
 
-        public static void logError(String message, Throwable throwable) {
-            logger.log(Level.SEVERE, message, throwable);
-        }
+
+    public static void logError(String message, Throwable throwable) {
+        logger.log(Level.SEVERE, message, throwable);
+    }
 }
